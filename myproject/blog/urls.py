e53f332c796629b85c4973c7b8fc29920b42342d
TaiDuongRepo <taiduong1301@gmail.com>
2024-03-14 22:23:16 +0700

    first commit

---
+++ myproject/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
